ec250130eea17ba5b271b8e31807edbd2e17e1c4
Ac-ob <157826104+Ac-ob@users.noreply.github.com>
2026-04-23 10:01:21 -0400

    Scatterplot_files

--- script.js
+++ script.js
@@ -209,3 +209,177 @@ function drawScatter(points, label) {
   ctx.textAlign = 'center';
   ctx.fillText(label, W / 2, H - 6);
 }
+
+
+
+//scatterplot with highlight
+const margin = 100; 
+const frameWidth = 1500; 
+const frameHeight = 700; 
+const visWidth = (frameWidth - 3*margin) / 2; 
+const visHeight = (frameHeight - 2*margin); 
+
+let frame = d3.select("#scatterplot_env")
+                .append("svg")
+                    .attr("width", frameWidth)
+                    .attr("height", frameHeight);
+
+let frame1 = frame.append('g')
+                    .attr("transform", "translate(" + margin + "," + margin + ")"); 
+let frame2 = frame.append('g')
+                    .attr("transform", "translate(" + (margin*2 + visWidth) + "," + margin + ")"); 
+
+// to plot
+let x1 = 'Water Temperature'
+let y1 = 'Dissolved O₂'
+
+let x2 = 'Water Temperature'
+let y2 = 'Dissolved O₂'
+
+//
+
+// plot function 
+
+
+
+//how to carch errors when loading json -> https://blog.stackademic.com/loading-json-data-in-d3-js-6859e7a71a1d
+
+d3.json("merged_data.json")
+        .then(function(data) {
+              
+
+  // set up axes
+    let xScale1 = d3.scaleLinear()
+                    .domain(d3.extent(data, d => d.waterTemp)).nice()
+                    .range([0, visWidth]);
+    let xScale2 = d3.scaleLinear()
+                    .domain(d3.extent(data, d => d.waterTemp)).nice()
+                    .range([0, visWidth]); 
+    let yScale1 = d3.scaleLinear()
+                    .domain(d3.extent(data, d => d.DO)).nice()
+                    .range([visHeight, 0]); 
+    let yScale2 = d3.scaleLinear()
+                    .domain(d3.extent(data, d => d.DO)).nice()
+                    .range([visHeight, 0]);
+    
+    let xAxis1 = d3.axisBottom(xScale1); 
+    let xAxis2 = d3.axisBottom(xScale2); 
+    let yAxis1 = d3.axisLeft(yScale1); 
+    let yAxis2 = d3.axisLeft(yScale2); 
+
+
+
+   // Add axes  
+    // scatter 1
+    frame1.append('g')
+            .attr('transform', 'translate(0,' + (visHeight) + ')')
+            .call(xAxis1)
+    frame1.append("text")             
+            .attr('transform', 'translate(' + (visWidth/2) + ',' + (visHeight + margin/2) + ')')
+            .style("text-anchor", "middle")
+            .text(x1);
+             
+    frame1.append('g')
+            .call(yAxis1); 
+    frame1.append("text")   
+            .attr("transform", "rotate(-90)")          
+            .attr('x', -visHeight/2)
+            .attr('y', -margin/2)
+            .style("text-anchor", "middle")
+            .text(y1);
+
+  // scatter 2
+    frame2.append('g')
+            .attr('transform', 'translate(0,' + (visHeight) + ')')
+            .call(xAxis2); 
+    frame2.append("text")             
+            .attr('transform', 'translate(' + (visWidth/2) + ',' + (visHeight + margin/2) + ')')
+            .style("text-anchor", "middle")
+            .text(x2);
+
+    frame2.append('g')
+            .call(yAxis2); 
+    frame2.append("text")   
+            .attr("transform", "rotate(-90)")          
+            .attr('x', -visHeight/2)
+            .attr('y', -margin/2)
+            .style("text-anchor", "middle")
+            .text(y2);
+
+
+   // set up color scale for feeding groups 
+    let feeding_colors = d3.scaleOrdinal()
+                            .domain(data.map(d => d['feedingGroup']))
+                            .range(['lightpink','red','blue','black', 'orange','green']); 
+
+
+
+ brush = d3.brush()
+                .extent([ // define what can be brushed 
+                    [d3.min(xScale1.range()), d3.min(yScale1.range())],
+                    [d3.max(xScale1.range()), d3.max(yScale1.range())]
+                ])
+                .on("brush end", (e) => { // event handler 
+                    if (e.selection === null) {
+                        circles = d3.selectAll('circle'); 
+                        circles = circles["_groups"][0]; 
+                        circles.forEach(c => { c.classList.remove('highlight'); })
+                    } else {
+                        const [[xMin, yMin], [xMax, yMax]] = e.selection; 
+                        data.map((d, i) => {
+                            
+                            selector = "._" + i;
+                            sel =  d3.selectAll(selector); 
+                            circles = sel["_groups"][0] // for each data in selection, find circles
+                        
+                            if (xMin <= xScale1(d[x1]) && xMax >= xScale1(d[x1]) &&
+                                yMin <= yScale1(d[y1]) && yMax >= yScale1(d[y1])) {
+                                circles.forEach(c => { c.classList.add('highlight'); }) // if circle is in selection, highlight
+                            } else {
+                                circles.forEach(c => { c.classList.remove('highlight'); })
+                            }
+                        })
+                    }
+                }); 
+
+  frame1.append("g")
+        .call(brush); 
+
+
+ let dot1 = frame1.append('g')
+            .selectAll("dot")
+            .data(data)
+            .enter()
+        .append("circle")
+               .attr("cx", function (d) { 
+                console.log(xScale1(d[x1]))
+                return xScale1(d[x1]); })
+            .attr("cy", function (d) { 
+                return yScale1(d[y1]); })
+            .attr("r", 20)
+            .style("fill", function (d) { 
+                return feeding_colors(d['feedingGroup']) 
+            })
+            .attr("class", (d, i) => {return "_" + i}); 
+
+  let dot2 = frame2.append('g')
+
+            .selectAll("dot")
+            .data(data)
+            .enter()
+        .append("circle")
+            .attr("cx", function (d) { 
+                return xScale2(d[x2]); })
+            .attr("cy", function (d) { 
+                return yScale2(d[y2]); })
+            .attr("r", 20)
+            .style("fill", function (d) { 
+                return feeding_colors(d['feedingGroup']) 
+            })
+            .attr("class", (d, i) => {return "_" + i});  
+
+
+console.log(frame1)
+console.log(data)
+
+  });
